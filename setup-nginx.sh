fad31fb83b137bddbe1414c4dd46d7ab9801646a
wilson99 <zhangweidong@hotmail.com>
2019-12-25 13:16:04 +0800

    test

--- setup-nginx.sh
+++ setup-nginx.sh
@@ -8,4 +8,4 @@ apt -y install nginx
 rm /var/www/html/index.nginx-debian.html
 
 # Create the default page for the region
-wget https://github.com/wilson99/mslearn-distribute-load-with-traffic-manager/index.$1.html -O /var/www/html/index.html
+wget https://github.com/wilson99/mslearn-distribute-load-with-traffic-manager/blob/master/index.$1.html -O /var/www/html/index.html
